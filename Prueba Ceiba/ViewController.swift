a2ad63d2068136e48f0ff414ecc07b91b98c19ac
horacio <horacio.solorio@gmail.com>
2022-09-03 11:05:11 -0500

    Initial Commit

---
+++ Prueba Ceiba/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prueba Ceiba
+//
+//  Created by Horacio Solorio on 03/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
